d136af0f5bb4f25c524d2be4263cb2b94731b5e8
Matsumura Toshiaki <t.matsuwest.ern@gmail.com>
2018-08-25 23:47:44 +0900

    update endpoint

--- src/api/url.js
+++ src/api/url.js
@@ -1,4 +1,7 @@
 export const BASE_URL = 'http://52.196.176.142'
 export const ENDPOINTS = {
-    signUp: '/api/auth/signup'
+    Lounge: "/api/lounge/",
+    Member: "/api/lounge/members",
+    Preference: "/api/pref",
+    Result: "/api/result"
 }
