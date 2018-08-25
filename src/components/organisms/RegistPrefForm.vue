39c50c1ab5b47d7b6560579237cb36b367967f41
田畑颯 <chinarra.tabata@gmail.com>
2018-08-25 21:31:07 +0900

    Create params

--- src/components/organisms/RegistPrefForm.vue
+++ src/components/organisms/RegistPrefForm.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   div
+    div(id="self-member-id" :data-self-member-id="$route.params.member_id")
     MemberList
     RegistPrefButton(@submit="registPref")
 </template>
@@ -11,7 +12,18 @@ import RegistPrefButton from '../atoms/RegistPrefButton'
 export default {
   methods: {
     registPref() {
-      console.log('Good!');
+      var prefNodes = document.getElementsByClassName('draggable-item');
+      var preferences = Array.prototype.map.call(prefNodes, function (prefNode) {
+        return prefNode.getAttribute('data-member-id');
+      });
+      var selfMemberId = document.getElementById('self-member-id')
+        .getAttribute('data-self-member-id');
+
+      var params = {
+        'member_id': selfMemberId,
+        'preferences': preferences
+      };
+      console.log(params);
     }
   },
   components: {
