bad734e2ce7196d6142da4bd2694e17efd1eaffa
田畑颯 <chinarra.tabata@gmail.com>
2018-08-25 23:57:11 +0900

    Submit data

--- src/page/CreateMember.vue
+++ src/page/CreateMember.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
   div(id="create-member")
-    h1 create member
-  div
-    img(:src="srcPath")
-    input(type="file" v-on:change="preview($event)" accept="image")
-    router-link(:to="{name: 'WaitingMember', params: {lounge_id: 12345}}") waiting member
+      form(id='form' @submit.prevent="register($event)")
+        img(:src="srcPath" id="thumbnail")
+        label 画像：
+          input(type="file" v-on:change="preview($event)" accept="image")
+        label 名前：
+          input(type="text" placeholder="Hayate Tabata" name="name" id="name")
+        button(type="submit") 登録
 </template>
 
 <script>
@@ -26,7 +28,13 @@ export default {
         this.srcPath = e.target.result;
       };
       this.srcPath = fileReader.readAsDataURL(file);
-    }
+    },
+    register (e) {
+      var form = document.forms.form;
+      var img = this.srcPath;
+      var name = form.name.value;
+      console.log(name);
+    },
   },
   created(){
     // when created
@@ -36,6 +44,23 @@ export default {
 </script>
 
 <style lang="stylus">
-
-
+.form {
+  width: 100%;
+}
+#thumbnail {
+  width: 50%;
+}
+label {
+  display: inline-block;
+}
+input {
+  display: inline-block;
+}
+img {
+  display: inline-block;
+}
+button {
+  display: inline-block;
+  width: 100%;
+}
 </style>
