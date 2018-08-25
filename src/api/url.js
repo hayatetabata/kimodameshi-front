9d70fd2413659d16ce97e1860407c9e35102e1b5
Matsumura Toshiaki <t.matsuwest.ern@gmail.com>
2018-08-25 12:35:57 +0900

    apiラッパーの追加

---
+++ src/api/url.js
@@ -0,0 +1,4 @@
+export const BASE_URL = 'http://52.196.176.142'
+export const ENDPOINTS = {
+    signUp: '/api/auth/signup'
+}
---
+++ src/api/util.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import * as url from './url'
+
+axios.interceptors.response.use(function (response) {
+    if (response.data.status !== '0') {
+        // status codeはresponse.status
+        return Promise.reject(response)
+    }
+    return response
+}, function (error) {
+    if (error.response.status === 401) {
+        localStorage.removeItem('session')
+        // location.href = '/signin'
+        return null
+    }
+    return Promise.reject(error)
+})
+
+export default {
+    createMethod(data, endpoint, needSession, method) {
+        let params = new URLSearchParams()
+        if (needSession) params.append('session', localStorage.getItem('session'))
+        params.append('data', JSON.stringify(data))
+        return axios({
+            method: 'post',
+            url: url.BASE_URL + endpoint,
+            data: params,
+            headers: { 'Q-HTTP-Method': method }
+        })
+    }
+}
