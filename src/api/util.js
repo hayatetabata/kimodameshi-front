53a8524a2fa0aceceb68be1a95404b8160baf8a3
Matsumura Toshiaki <t.matsuwest.ern@gmail.com>
2018-08-25 23:48:35 +0900

    update createRequest funcntion

--- src/api/util.js
+++ src/api/util.js
@@ -1,29 +1,13 @@
 import axios from 'axios'
 import * as url from './url'
 
-axios.interceptors.response.use(function (response) {
-    if (response.data.status !== '0') {
-        // status codeはresponse.status
-        return Promise.reject(response)
-    }
-    return response
-}, function (error) {
-    if (error.response.status === 401) {
-        localStorage.removeItem('session')
-        // location.href = '/signin'
-        return null
-    }
-    return Promise.reject(error)
-})
-
-export function createMethod(data, endpoint, needSession, method) {
-    let params = new URLSearchParams()
-    if (needSession) params.append('session', localStorage.getItem('session'))
-    params.append('data', JSON.stringify(data))
-    return axios({
-        method: 'post',
+export function createRequest(data, endpoint, method) {
+    let params = new URLSearchParams(data)
+    let config = {
+        method: method,
         url: url.BASE_URL + endpoint,
-        data: params,
-        headers: { 'Q-HTTP-Method': method }
-    });
-};
+    }
+    let key = (method == "GET") ? "params" : "data" ;
+    config[key] = params
+    return axios(config);
+}
