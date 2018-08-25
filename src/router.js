ef1974aafe601aa80be0409d6e7dfb310f458272
Matsumura Toshiaki <t.matsuwest.ern@gmail.com>
2018-08-25 16:28:36 +0900

    パスの変更+α

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import NotFoundComponent from './views/NotFoundComponent.vue'
+import Home from './page/Home.vue'
+import NotFoundComponent from './page/NotFoundComponent.vue'
 
 Vue.use(Router)
 
@@ -15,12 +15,12 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
+      path: '/lounge/',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './page/CreateLounge.vue')
     },
     { path: '*', component: NotFoundComponent }
   ]
