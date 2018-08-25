c90087ab760a649d84b8c08c82eb513e247db016
Matsumura Toshiaki <t.matsuwest.ern@gmail.com>
2018-08-25 13:58:21 +0900

    rewrite with pug & stylus

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div(class="home")
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
@@ -16,3 +15,7 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+
+</style>
