2e372f634bced47add9eb08cddcf2835bbdd35f8
田畑颯 <chinarra.tabata@gmail.com>
2018-08-25 23:21:27 +0900

    Upload image

--- src/page/CreateMember.vue
+++ src/page/CreateMember.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
   div(id="create-member")
     h1 create member
+  div
+    img(:src="srcPath")
+    input(type="file" v-on:change="preview($event)" accept="image")
     router-link(:to="{name: 'WaitingMember', params: {lounge_id: 12345}}") waiting member
 </template>
 
@@ -9,14 +12,21 @@ export default {
   name: "CreateMember",
   data(){
     return {
-      // data
+      srcPath: "http://localhost:8000/test.jpg",
     }
   },
   props: {
     // props
   },
   methods: {
-    // functions
+    preview (e) {
+      var file = e.target.files[0];
+      var fileReader = new FileReader();
+      fileReader.onload = (e) =>  {
+        this.srcPath = e.target.result;
+      };
+      this.srcPath = fileReader.readAsDataURL(file);
+    }
   },
   created(){
     // when created
