613c62f31bdb9d22d661796d1f5fa32a9ba49264
田畑颯 <chinarra.tabata@gmail.com>
2018-08-26 15:22:53 +0900

    Fix url

--- src/api/url.js
+++ src/api/url.js
@@ -1,7 +1,7 @@
 export const BASE_URL = 'http://192.168.11.29:3000'
 export const ENDPOINTS = {
     Lounge: "/api/lounge/",
-    Member: "/api/lounge/member",
+    Member: "/api/lounge/members",
     Preference: "/api/pref",
     Result: "/api/result"
 }
