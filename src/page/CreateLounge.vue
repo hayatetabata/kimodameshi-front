e43f0cbe1cc812c53b343332e5ac3b18995e23f9
田畑颯 <chinarra.tabata@gmail.com>
2018-08-26 10:02:57 +0900

    Design on provisional

--- src/page/CreateLounge.vue
+++ src/page/CreateLounge.vue
@@ -1,6 +1,22 @@
 <template lang="pug">
   div(id="create-lounge")
     h1 create lounge
+    form(id="form")
+      input(type="text" name="name" placeholder="ラウンジ名")
+      div(id="options")
+        div(class="owner field")
+          input(type="checkbox" name="join_owner")
+          label オーナーも参加する
+
+      input(type="text" name="owner_name" placeholder="自分の名前")
+
+      div(class="group field")
+        input(type="text" name="first" placeholder="例：男性")
+        input(type="text" name="second" placeholder="例：女性")
+
+      input(type="checkbox" name="allow_alone")
+      label 一人を許容する
+
     router-link(:to="{name: 'CreateMember', params: {lounge_id: 12345}}") Create Member!
 </template>
 
@@ -26,6 +42,12 @@ export default {
 </script>
 
 <style lang="stylus">
+label {
+  font-size: 10px;
+}
 
-
+.field {
+  width: 100px;
+  margin: 0 auto 20px;
+}
 </style>
