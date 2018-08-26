755ae88142566f64fbd031ce4ba6c2e4473b0411
田畑颯 <chinarra.tabata@gmail.com>
2018-08-26 09:43:37 +0900

    Segue to create lounge

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,18 +1,12 @@
 <template lang="pug">
   div(class="home")
-    img(alt="Vue logo" src="../assets/logo.png")
-    HelloWorld(msg="Welcome to Your Vue.js App")
+    h1 肝試しマッチング
+    router-link(:to="{name: 'CreateLounge'}") Create Lounge
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
