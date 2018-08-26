b6bf9710dd18b65242f10d19944129e5e9a7c549
Matsumura Toshiaki <t.matsuwest.ern@gmail.com>
2018-08-26 19:53:26 +0900

    スタイルの更新

--- src/page/WaitingResult.vue
+++ src/page/WaitingResult.vue
@@ -1,7 +1,15 @@
 <template lang="pug">
-  div
-    h1 submitted!!
-    router-link(
-        :to="{name: 'Result', params: {lounge_id: 12345, member_id: 12345}}"
-    ) Show Result
+  div.wrapper
+    div.centered
+        p あと<span class="isBig">8</span>人の
+        p 登録を待っています。
 </template>
+
+<style lang="stylus" scoped>
+p
+    line-height 1rem
+    text-align center
+    margin 1rem 0
+span
+    font-size 8rem
+</style>
