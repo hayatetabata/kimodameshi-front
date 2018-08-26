5c70d21bd145f6fc3aa7bc5494504b0f1c39d05b
田畑颯 <chinarra.tabata@gmail.com>
2018-08-26 17:48:56 +0900

    Show result

--- src/page/Result.vue
+++ src/page/Result.vue
@@ -3,32 +3,58 @@
     h1 Show result
     PartnerProfile(
     v-for="partner in partners"
-    :name="partner.name"
+    :name="partner.member_name"
     :src-path="partner.thumbnail_url"
     )
 </template>
 
 <script>
 import PartnerProfile from '@/components/molecules/PartnerProfile'
+import {createRequest} from '@/api/util'
+import {ENDPOINTS} from '@/api/url'
+
 
 export default {
   name: "Result",
   data () {
     return {
-      partners: [
-        {name: 'Matsumura Toshiaki', thumbnail_url: 'http://localhost:8080/Default_Profile_Image.png'},
-        {name: 'Matsumura Toshiaki', thumbnail_url: 'http://localhost:8080/Default_Profile_Image.png'},
-      ]
+      intervalId: null,
+      partners: []
     }
   },
-  props: {
-    // props
-  },
+  props: [
+      'lounge_id', 'member_id'
+  ],
   methods: {
     // functions
   },
   created(){
-    // when created
+    var params = {
+      'lounge_uuid': this.lounge_id,
+      'member_uuid': this.member_uuid,
+    };
+    var vue = this;
+    this.intervalId = window.setInterval(function () {
+      createRequest(params, ENDPOINTS.Result, 'GET')
+        .then((response) => {
+            if (response.data['matched_with'] != null) {
+                vue.partners = response.data['matched_with'];
+            }
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        })
+    }, 2000);
+  },
+  watch: {
+    partners: function (val) {
+        clearInterval(this.intervalId);
+    }
+  },
+  beforeDestroy () {
+    console.log('clearInterval')
+    clearInterval(this.intervalId)
   },
   components: {
     PartnerProfile
