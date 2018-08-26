0fcab8f5667a58576327e015e7aa91a258d64842
田畑颯 <chinarra.tabata@gmail.com>
2018-08-26 17:49:02 +0900

    Add function

--- src/page/WaitingResult.vue
+++ src/page/WaitingResult.vue
@@ -5,3 +5,20 @@
         :to="{name: 'Result', params: {lounge_id: 12345, member_id: 12345}}"
     ) Show Result
 </template>
+
+<script>
+export default {
+  name: "WaitingResult",
+  data(){
+    return {
+      intervalId: null,
+    }
+  },
+  props: [
+    'lounge_id', 'member_id'
+  ],
+  created () {
+
+  },
+}
+</script>
