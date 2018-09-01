5254876cacd3d21f2ac622e5e8891b573cc395f5
田畑颯 <chinarra.tabata@gmail.com>
2018-09-01 15:14:28 +0900

    Fix state

--- src/store/state.js
+++ src/store/state.js
@@ -2,17 +2,17 @@ export const state = {
   members: [
     {
       member_uuid: 'oajegfoq23efja',
-      member_thumbnail_url: 'http://192.168.24.151:8080/Default_Profile_Image.png',
+      thumbnail_url: 'http://192.168.24.151:8080/Default_Profile_Image.png',
       member_name: 'matsumura',
     },
     {
       member_uuid: 'fjfja;jiegjaw',
-      member_thumbnail_url: 'http://192.168.24.151:8080/Default_Profile_Image.png',
+      thumbnail_url: 'http://192.168.24.151:8080/Default_Profile_Image.png',
       member_name: 'tabata',
     },
     {
       member_uuid: ':gawej;oaihf',
-      member_thumbnail_url: 'http://192.168.24.151:8080/Default_Profile_Image.png',
+      thumbnail_url: 'http://192.168.24.151:8080/Default_Profile_Image.png',
       member_name: 'ando',
     },
   ],
