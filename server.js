db9173caa221eace9e9853cfbaf50a2ef29e17eb
田畑颯 <chinarra.tabata@gmail.com>
2018-09-02 19:40:04 +0900

    Forward to index

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var serveStatic = require('serve-static')
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.get(/.*/, function(req, res) {
+    res.sendfile(__dirname + "/dist/index.html");
+});
 var port = process.env.PORT || 5000;
 app.listen(port)
 console.log('server started ' + 5000);
